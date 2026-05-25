ce2dae9d9ad870f4b2292dfbc67a5edcf0575b6a
raj <rajudruva2@gmail.com>
2026-05-25 17:59:59 +0000

    modified App.tsx

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,7 +1,26 @@
+
 import { createApp } from '@backstage/frontend-defaults';
+import { githubAuthApiRef } from '@backstage/core-plugin-api';
+import { SignInPage } from '@backstage/core-components';
+
 import catalogPlugin from '@backstage/plugin-catalog/alpha';
 import { navModule } from './modules/nav';
 
 export default createApp({
+  components: {
+    SignInPage: props => (
+      <SignInPage
+        {...props}
+        auto
+        provider={{
+          id: 'github-auth-provider',
+          title: 'GitHub',
+          message: 'Sign in using GitHub',
+          apiRef: githubAuthApiRef,
+        }}
+      />
+    ),
+  },
+
   features: [catalogPlugin, navModule],
 });
